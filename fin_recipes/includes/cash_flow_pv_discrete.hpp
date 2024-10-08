4f76f1019a21ae7135175021e84ab7344718cb82
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2024-10-08 10:59:21 +0100

    refactoring the library

--- fin_recipes/includes/cash_flow_pv_discrete.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <cmath>
-#include <vector>
-#include <iostream>
-#include <cassert>
-
-double cash_flow_pv_discrete(const std::vector<double> &, const std::vector<double> &, const double &);
