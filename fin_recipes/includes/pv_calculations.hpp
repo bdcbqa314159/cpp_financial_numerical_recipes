6c408827d0fd9205471c2604fc161bf66f3efcfb
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2024-10-11 00:40:58 +0100

    regrouping all the previous pv calculations

--- fin_recipes/includes/pv_calculations.hpp
+++ fin_recipes/includes/pv_calculations.hpp
@@ -6,3 +6,12 @@
 #include "definitions.hpp"
 
 double cash_flow_pv_discrete(const v_double &, const v_double &, const double &);
+double cash_flow_pv(const v_double &cflow_times, const v_double &, const double &);
+
+const double ERROR = -1e30;
+
+double cash_flow_irr_discrete(const v_double &, const v_double &);
+
+inline int sgn(const double &r) { return r >= 0 ? 1 : -1; }
+
+bool cash_flow_unique_irr(const v_double &, const v_double &);
