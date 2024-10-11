0a692dde92b61fb82657608838e0e4588bc2a7f7
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2024-10-11 18:17:41 +0100

    adding a new function

--- fin_recipes/includes/term_ir.hpp
+++ fin_recipes/includes/term_ir.hpp
@@ -2,4 +2,5 @@
 
 double term_structure_yield_from_discount_factor(const double &, const double &);
 double term_structure_discount_factor_from_yield(const double &, const double &);
+double term_structure_forward_rate_from_discount_factors(const double &, const double &, const double &);
 double term_structure_forward_rate_from_yields(const double &, const double &, const double &, const double &);
--- fin_recipes/src/term_ir.cpp
+++ fin_recipes/src/term_ir.cpp
@@ -9,6 +9,10 @@ double term_structure_discount_factor_from_yield(const double &y, const double &
     return std::exp(-y * t);
 }
 
+double term_structure_forward_rate_from_discount_factors(const double &d1, const double &d2, const double &t){
+    return std::log(d1 / d2) / t;
+}
+
 double term_structure_forward_rate_from_yields(const double &y1, const double &y2, const double &t1, const double &t2){
     return (y2 * t2 - y1 * t1) / (t2 - t1);
 }
